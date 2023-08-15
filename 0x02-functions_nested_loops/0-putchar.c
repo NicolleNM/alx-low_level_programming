a60975d2f587362aa89504d7d9936d68a14557db
NicolleNM <n.marizani@alustudent.com>
2023-08-15 11:56:55 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -20,5 +17,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-
 }
