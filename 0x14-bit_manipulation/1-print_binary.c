887f72d41c062e1ffd3caaca0f49390c0d60eb5f
nmarizani <n.marizani@alustudent.com>
2023-09-28 09:07:52 -0700

    Updated

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int v, count = 0;
+	unsigned long int current;
+
+	for (v = 63; v >= 0; v--)
+	{
+		current = n >> v;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+
